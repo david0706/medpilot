9e829e15517b5d1714a8f5ff5c22e85829413ef5
David Ghouzi <davidghouzi@hotmail.com>
2025-09-08 16:47:11 +0200

    mini updates tab web icons + Text

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -6,11 +6,17 @@ import { cn } from "@/lib/utils";
 import { Footer } from "@/components/footer";
 
 export const metadata: Metadata = {
-  title: "Everything AI",
+  title: "MedPilot",
   description:
-    "Everything AI is a platform that provides a wide range of AI tools and services to help you stay on top of your business. Generate images, text and everything else that you need to get your business off the ground.",
+    "Medpilot est un outil qui vous permet de rédiger 5 plus rapidement vos rapports médicaux radiologiques grâce à une dictée intelligente",
+  icons: {
+      icon: '/favicon.ico',
+      apple: '/apple-touch-icon.png',
+  },
   openGraph: {
-    images: ["https://ai-saas-template-aceternity.vercel.app/banner.png"],
+      title: 'MedPilot',
+      description: 'Dictée médicale intelligente pour radiologues',
+      images: ['/logos/medpilot-icon.png'],
   },
 };
 
